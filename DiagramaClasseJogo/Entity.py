b2fbcfce96ad872a5ebd2ed1fef67496b2e10823
Gustavo Oliveira <comediagusta@gmail.com>
2024-03-12 21:01:01 -0300

    Signed-off-by: Gustavo Oliveira <comediagusta@gmail.com>

---
+++ DiagramaClasseJogo/Entity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Entity(Entity):
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Player.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Player import Player
+from Entity import Entity
+from Player import Player
+from Entity import Entity
+
+class Player(Player, Entity, Player, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
